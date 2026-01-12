c2b529e81ecf1e5dfb90dcb1c1b6c7e5aab35668
Arsenii Kvachan <arsenii@kvachan.com>
2026-01-12 01:42:07 +0100

    [Docs] Explain table creation

--- contributing.md
+++ contributing.md
@@ -1,3 +1,54 @@
+## Setup PGSQL
+
+Use this set of SQL queries to create development and test DBs:
+```sql
+CREATE DATABASE <db_name>;
+CREATE SCHEMA IF NOT EXISTS general;
+CREATE TABLE IF NOT EXISTS general.users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(512) NOT NULL UNIQUE,
+    user_name VARCHAR(64) NOT NULL UNIQUE,
+    full_name VARCHAR(128) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS general.candidates (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES general.users(id) ON DELETE CASCADE,
+    about TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS general.recruiters (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES general.users(id) ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS general.positions (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    company TEXT
+);
+CREATE TYPE reaction_type AS ENUM ('like', 'dislike');
+CREATE TABLE IF NOT EXISTS general.candidates_reactions (
+    candidate_id INT NOT NULL REFERENCES general.candidates(id) ON DELETE CASCADE,
+    position_id INT NOT NULL REFERENCES general.positions(id) ON DELETE CASCADE,
+    reaction_type reaction_type NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (candidate_id, position_id)
+);
+CREATE TABLE IF NOT EXISTS general.recruiters_reactions (
+    recruiter_id INT NOT NULL REFERENCES general.recruiters(id) ON DELETE CASCADE,
+    position_id INT NOT NULL REFERENCES general.positions(id) ON DELETE CASCADE,
+    candidate_id INT NOT NULL REFERENCES general.candidates(id) ON DELETE CASCADE,
+    reaction_type reaction_type NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (recruiter_id, position_id, candidate_id)
+);
+CREATE TABLE IF NOT EXISTS general.matches (
+    candidate_id INT NOT NULL REFERENCES general.candidates(id) ON DELETE CASCADE,
+    position_id INT NOT NULL REFERENCES general.positions(id) ON DELETE CASCADE,
+    timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (candidate_id, position_id)
+);
+```
+
 ## Installation
 - Inside the root of the repository:
 ```
--- tests/integrations_test.go
+++ tests/integrations_test.go
@@ -58,63 +58,6 @@ func TestMain(m *testing.M) {
 
 	hirevec.HirevecDatabase = testDB
 
-	_, err = testDB.Exec(`
-	CREATE SCHEMA IF NOT EXISTS general;
-
-	CREATE TABLE IF NOT EXISTS general.users (
-		id SERIAL PRIMARY KEY,
-		email VARCHAR(512) NOT NULL UNIQUE,
-		user_name VARCHAR(64) NOT NULL UNIQUE,
-		full_name VARCHAR(128) NOT NULL
-	);
-
-	CREATE TABLE IF NOT EXISTS general.candidates (
-		id SERIAL PRIMARY KEY,
-		user_id INT NOT NULL REFERENCES general.users(id) ON DELETE CASCADE,
-		about TEXT NOT NULL
-	);
-
-	CREATE TABLE IF NOT EXISTS general.recruiters (
-		id SERIAL PRIMARY KEY,
-		user_id INT NOT NULL REFERENCES general.users(id) ON DELETE CASCADE
-	);
-
-	CREATE TABLE IF NOT EXISTS general.positions (
-		id SERIAL PRIMARY KEY,
-		title TEXT NOT NULL,
-		description TEXT NOT NULL,
-		company TEXT
-	);
-
-	CREATE TABLE IF NOT EXISTS general.candidates_reactions (
-		candidate_id INT NOT NULL REFERENCES general.candidates(id) ON DELETE CASCADE,
-		position_id INT NOT NULL REFERENCES general.positions(id) ON DELETE CASCADE,
-		reaction_type reaction_type NOT NULL,
-		created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-		PRIMARY KEY (candidate_id, position_id)
-	);
-
-	CREATE TABLE IF NOT EXISTS general.recruiters_reactions (
-		recruiter_id INT NOT NULL REFERENCES general.recruiters(id) ON DELETE CASCADE,
-		position_id INT NOT NULL REFERENCES general.positions(id) ON DELETE CASCADE,
-		candidate_id INT NOT NULL REFERENCES general.candidates(id) ON DELETE CASCADE,
-		reaction_type reaction_type NOT NULL,
-		created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-		PRIMARY KEY (recruiter_id, position_id, candidate_id)
-	);
-
-	CREATE TABLE IF NOT EXISTS general.matches (
-		candidate_id INT NOT NULL REFERENCES general.candidates(id) ON DELETE CASCADE,
-		position_id INT NOT NULL REFERENCES general.positions(id) ON DELETE CASCADE,
-		timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
-		PRIMARY KEY (candidate_id, position_id)
-	);
-	`)
-	if err != nil {
-		fmt.Println("failed to create schema/tables:", err)
-		os.Exit(1)
-	}
-
 	ts := httptest.NewServer(hirevec.GetMainHandler())
 	defer ts.Close()
 	baseURL = ts.URL + "/api/v0"
